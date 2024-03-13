b1327a18a33499329fe60b712ec4be2b5eda739a
JustSlow27 <guadamuz277@gmail.com>
2024-03-13 15:08:13 -0600

    workshop 4 JS added

---
+++ workshop_4/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.getElementById('checkbox');
+    checkbox.checked = localStorage.getItem('darkMode') === 'true';
+    document.documentElement.setAttribute('data-theme', checkbox.checked ? 'dark' : 'light');
+
+    checkbox.addEventListener('change', function() {
+        document.documentElement.setAttribute('data-theme', this.checked ? 'dark' : 'light');
+        localStorage.setItem('darkMode', this.checked.toString());
+    });
+
+    const saveClientButton = document.getElementById('saveClient'); 
+    saveClientButton.addEventListener('click', saveClient);
+});
+
+function saveClient() {
+    const firstName = document.getElementById('firstName').value; 
+    const lastName = document.getElementById('lastName').value; 
+    const phone = document.getElementById('phone').value; 
+    const client = { firstName, lastName, phone }; 
+    const clients = JSON.parse(localStorage.getItem('clients')) || []; 
+    clients.push(client);
+    localStorage.setItem('clients', JSON.stringify(clients)); 
+
+    document.getElementById('firstName').value = ''; 
+    document.getElementById('lastName').value = ''; 
+    document.getElementById('phone').value = ''; 
+    alert('Cliente guardado con éxito');
+}
